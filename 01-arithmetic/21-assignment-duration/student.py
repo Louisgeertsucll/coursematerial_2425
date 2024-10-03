5b7eff783fdf579de5e6429222642bd46ca06ee5
louis geerts <r1058208@ucll.be>
2024-10-03 12:55:19 +0200

    update

--- 01-arithmetic/21-assignment-duration/student.py
+++ 01-arithmetic/21-assignment-duration/student.py
@@ -1,1 +1,15 @@
 # write your code here
+def hour(durataion):
+    h=(3600)
+    return (durataion//h)
+
+def minutes(duration):
+    h=(hour(duration))
+    m=  (duration)-(3600*h)
+    return  (m//60)
+
+def second(duration):
+   h=(hour(duration))
+   m=(duration)-(h*3600)
+   s=(duration)-(m*60)
+   return s
